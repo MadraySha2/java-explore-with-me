eab2bd2c9a876b32b79749733619ef1a47de8a43
MadraySha2 <114656140+MadraySha2@users.noreply.github.com>
2023-07-02 12:22:09 +0300

    Update StatsRepository.java

--- stats-service/server/src/main/java/ru/practicum/server/repository/StatsRepository.java
+++ stats-service/server/src/main/java/ru/practicum/server/repository/StatsRepository.java
@@ -10,14 +10,14 @@ import java.util.List;
 
 public interface StatsRepository extends JpaRepository<Hit, Long> {
 
-    @Query("select new OutStats(h.app, h.uri, count(h.ip))" +
+    @Query("select new ru.practicum.server.model.OutStats(h.app, h.uri, count(h.ip)) " +
             "from Hit h " +
             "where h.timestamp between ?1 and ?2 " +
             "group by h.app, h.uri " +
             "order by count(h.ip) desc")
     List<OutStats> findAllWithoutUris(LocalDateTime start, LocalDateTime end);
 
-    @Query("select new OutStats(h.app, h.uri, count(h.ip))" +
+    @Query("select new ru.practicum.server.model.OutStats(h.app, h.uri, count(h.ip)) " +
             "from Hit h " +
             "where h.timestamp between ?1 and ?2 " +
             "and h.uri in (?3) " +
@@ -25,7 +25,7 @@ public interface StatsRepository extends JpaRepository<Hit, Long> {
             "order by count(h.ip) desc")
     List<OutStats> findAllNotUnique(LocalDateTime start, LocalDateTime end, List<String> uris);
 
-    @Query("select new OutStats(h.app, h.uri, count(distinct h.ip))" +
+    @Query("select new ru.practicum.server.model.OutStats(h.app, h.uri, count(distinct h.ip)) " +
             "from Hit h " +
             "where h.timestamp between ?1 and ?2 " +
             "and h.uri in (?3) " +
