84bf41fb8b12c32cc99dfb39a938b2c1439ef057
MadraySha2 <114656140+MadraySha2@users.noreply.github.com>
2023-07-20 00:43:51 +0300

    Update CompilationService.java

--- main-service/src/main/java/ru/practicum/service/CompilationService.java
+++ main-service/src/main/java/ru/practicum/service/CompilationService.java
@@ -14,7 +14,6 @@ import ru.practicum.model.Event;
 import ru.practicum.repository.CompilationRepository;
 import ru.practicum.repository.EventRepository;
 
-import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
